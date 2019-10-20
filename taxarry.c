7c873e380844ae6143a894f96ca907009c212a0a
Razent1 <53121795+Razent1@users.noreply.github.com>
2019-10-20 18:31:10 +0300

    Update taxarry.c

--- taxarry.c
+++ taxarry.c
@@ -12,33 +12,34 @@ float taxIsr(float salary, float taxAllowance)
 	float tax = -taxAllowance * 216;
 	int i;
 	float res;
-	int range1[] = {0, 6310, 9050, 14530, 20200, 42030, 54130};
+	int range1[] = {0, 6310, 9050, 14530, 20200, 42030, 54130, 2147483647};
 	float range2[] = {0.1, 0.14, 0.2, 0.31, 0.35, 0.47, 0.5};
 
-	for(i = 0; salary > range1[i]; i++)
+	for(i = 0; salary >= range1[i]; i++)
 	{
 		res = tax + (salary - range1[i]) * range2[i];
-		if (salary >= range1[i] || salary < range1[i + 1])
+		if (salary >= range1[i] && salary < range1[i + 1])
 		return res > 0? res : 0;
 		else
 		tax = tax + (range1[i + 1] - range1[i]) * range2[i];
 	}
 	if(tax > 0)
-	return tax + (salary - 54130) * 0.5;
+	return tax;
 	else
 	return 0;
 	}
 
 float newsalary(float salary)
 {
-	return salary - taxIsr(150000, 5.25);
+	return salary - taxIsr(66000, 5.25);
 	}
+
 
 int main()
 {
-	float salary = newsalary(150000);
-	 printf("salary = %f\n", salary);
-	 float tax = taxIsr(150000, 5.25);
-	 printf("tax = %f\n", tax);
+	float salary = newsalary(66000);
+	printf("salary = %f\n", salary);
+	float tax = taxIsr(66000, 5.25);
+	printf("tax = %f\n", tax);
 	return 0;
 	}
